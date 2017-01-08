f589e4338114fb52ba916b956f7db01c0234f932
NadimRai <rai_nadim@yahoo.com>
2017-01-07 18:55:12 -0800

    Cleaned up Article Index Show page and Comment Show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,9 @@ class PagesController < ApplicationController
   
   def index
     if current_user
-      redirect_to articles_path
-    end
-    @articles = Article.last(5)
+ redirect_to articles_path
+ end
+ @articles = Article.paginate(page: params[:page], per_page: 5).order("created_at DESC")
   end
 
   def contact
